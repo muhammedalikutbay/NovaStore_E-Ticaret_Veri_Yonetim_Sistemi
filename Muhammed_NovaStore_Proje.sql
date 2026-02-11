fb6f263ccc0a25543ee863c68fffda00358d74e7
Muhammed Ali KUTBAY <muhammedalikutbay@gmail.com>
2026-02-11 12:37:44 +0300

    Customers tablosu oluşturuldu

--- Muhammed_NovaStore_Proje.sql
+++ Muhammed_NovaStore_Proje.sql
@@ -7,9 +7,18 @@ USE NovaStoreDB;
 
 GO
 -- Tabloların Oluşturulması
--- Kategoriler Tablosu
+-- Categories Tablosu
 CREATE TABLE
     Categories (
         CategoryID INT IDENTITY (1, 1) PRIMARY KEY,
         CategoryName VARCHAR(50) NOT NULL
+    );
+
+-- Customers Tablosu
+CREATE TABLE
+    Customers (
+        CustomerID INT IDENTITY (1, 1) PRIMARY KEY,
+        FullName VARCHAR(50),
+        City VARCHAR(20),
+        Email VARCHAR(100) UNIQUE
     );
