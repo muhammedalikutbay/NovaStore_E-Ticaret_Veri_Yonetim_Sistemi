fe1e733440781b8c7dcb07889b123e0a68b1ef02
Muhammed Ali KUTBAY <muhammedalikutbay@gmail.com>
2026-02-11 12:35:23 +0300

    Categories tablosu oluşturuldu

--- Muhammed_NovaStore_Proje.sql
+++ Muhammed_NovaStore_Proje.sql
@@ -1,2 +1,15 @@
 -- Veri Tabanı Oluşturma
 CREATE DATABASE NovaStoreDB;
+
+GO
+-- Veri Tabanını Kullanma
+USE NovaStoreDB;
+
+GO
+-- Tabloların Oluşturulması
+-- Kategoriler Tablosu
+CREATE TABLE
+    Categories (
+        CategoryID INT IDENTITY (1, 1) PRIMARY KEY,
+        CategoryName VARCHAR(50) NOT NULL
+    );
